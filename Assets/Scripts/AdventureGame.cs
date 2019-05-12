16c9d455d25f266e2627ff7c6c6fc4fe9bb58265
xer-xes <31579450+xer-xes@users.noreply.github.com>
2019-05-12 13:46:37 +0530

    Prototype

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI storyText;
+    [SerializeField] State First_State;
+
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = First_State;
+        storyText.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageStates();
+    }
+
+    private void ManageStates()
+    {
+        var NextStates = state.GetNextStates();
+        for (int i = 0; i < NextStates.Length;i++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = NextStates[i];
+            }
+        }
+        storyText.text = state.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,14)][SerializeField] string StoryText;
+    [SerializeField] State[] NextStates;
+    public string GetStateStory()
+    {
+        return StoryText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return NextStates;
+    }
+}
